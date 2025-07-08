bbadc7855b77887ee16c31f68ba2b9098c07cfe3
kev <jkevinm13@gmail.com>
2025-07-08 09:57:49 -0400

    feat: formulario de usuarios completo

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,8 +1,29 @@
+'use client'
+
 import { getUsers } from '@/actions/users'
 import { UserPageComponent } from '@/app/admin/users/page-component'
+import { CreateOrUpdateUserSchema } from '@/app/admin/users/schema'
 
 export default async function UsersPage() {
-  const users = await getUsers()
+  const usersRaw = await getUsers()
+
+  const users: CreateOrUpdateUserSchema[] = usersRaw.map(user => ({
+    id: user.id,
+    name: user.name,
+    ap_paterno: user.ap_paterno,
+    ap_materno: user.ap_materno,
+    email: user.email,
+    ci: user.ci,
+    expedido_en: user.expedido_en,
+    fecha_nacimiento: user.fecha_nacimiento,
+    phone: user.phone ?? '',
+    address: user.address ?? '',
+    avatar_url: user.avatar_url ?? '',
+    password: '',
+    confirmPassword: '',
+    type: user.type as 'USER' | 'ADMIN' | 'PROVEEDOR',
+    intent: 'update',
+  }))
 
   return <UserPageComponent users={users} />
 }
